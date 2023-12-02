563a82595044e941081a272e0123a06b3aa13e21
MathiasHarrer <mathias.h.harrer@gmail.com>
2023-12-02 14:21:09 +0100

    minify

--- public/zenodo.js
+++ public/zenodo.js
@@ -13,6 +13,24 @@ if (sessionStorage.getItem("apiResponse") === null) {
     async function saveApiResponse() {
       let data = await fetchData();
       sessionStorage.setItem("apiResponse", JSON.stringify(data));
+      sessionStorage.setItem('dataSaved', 'true');
     }
     saveApiResponse();
+}
+
+// random id generator function
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// check if data is loaded function
+function isDataSaved() {
+  return sessionStorage.getItem("dataSaved") === 'true';
 }
